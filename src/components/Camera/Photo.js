2d3905d00a44e9f01ace6a950da7212bd705db72
Elías <earayad@hotmail.cl>
2020-04-27 04:02:00 -0400

    test commit

--- src/components/Camera/Photo.js
+++ src/components/Camera/Photo.js
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
     alignItems:'center'
   },
   insideBall:{
-    backgroundColor: '#ffff',
+    backgroundColor: '#fff',
     borderRadius: (width),
     height:(width/7),
     width: (width/7),
